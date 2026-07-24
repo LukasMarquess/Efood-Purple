e5456875c0989e2102f6ddf93e77cf9d98cea3e9
Lucas Marques <lucasmarquesgamer@gmail.com>
2026-07-24 11:36:42 -0300

    Aplicando fonte

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', sans-serif; /* Você pode importar a fonte no index.html depois */
+    font-family: 'Roboto';
   }
 
   body {
